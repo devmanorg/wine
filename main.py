72f5e8cdc7035bff5c1710e26a8788dc2c98073e
Nurbek K <nurbek@inbox.ru>
2019-09-12 12:25:07 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import json
+import itertools
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+
+env = Environment(
+    loader=FileSystemLoader(''),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('index.html')
+
+html = template.render()
+
+with open('index.html', 'w') as file:
+    file.write(html)
+
+
+server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+server.serve_forever()
