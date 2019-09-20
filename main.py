8c10408907e538ab4baf63aa863ff85956a2b7e1
voron434 <070809010@mail.ru>
2019-09-20 18:09:01 +0300

    Remove jinja2 part

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-from jinja2 import Environment, FileSystemLoader, select_autoescape
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-
-
-env = Environment(
-    loader=FileSystemLoader(''),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('index.html')
-
-html = template.render()
-
-with open('index.html', 'w') as file:
-    file.write(html)
 
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
